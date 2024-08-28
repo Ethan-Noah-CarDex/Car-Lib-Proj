1bcb11e11948221e48071ad5b39057e4a0c874b3
Noah <neglymph99@gmail.com>
2024-08-28 11:16:29 -0400

    final version of the project

--- Car Library/src/components/Ccard.jsx
+++ Car Library/src/components/Ccard.jsx
@@ -1,14 +1,16 @@
 const Carcard = ({ car }) => {
-   
-    return (
+   const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(`${car.Make_Name} ${car.Model_Name}`)}&tbm=isch`;
+
+   return (
        <div className="individualcard">
         <h2>{car.Make_Name}</h2>
         <p>{car.Model_Name}</p>
         <a href={googleSearchUrl} target="_blank" rel="noopener noreferrer">
-        <button>Images</button>
+        <button class="button-73" role="button">See more...</button>
+
       </a>       </div>
     )
-      
+
    }
 
 
@@ -16,18 +18,18 @@ const Carcard = ({ car }) => {
 
  //import React from 'react';
 
- const Card = ({ searchTerm }) => {
-   // Build the Google Image search URL
-  const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchTerm)}&tbm=isch`;
+//  const Card = ({ searchTerm }) => {
+//    // Build the Google Image search URL
+//   const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchTerm)}&tbm=isch`;
 
-   return (
-     <div className="card">
-       <h3>{searchTerm}</h3>
-       <a href={googleSearchUrl} target="_blank" rel="noopener noreferrer">
-         <button>Search for {searchTerm} Images</button>
-       </a>
-     </div>
-   );
- };
+//    return (
+//      <div className="card">
+//        <h3>{searchTerm}</h3>
+//        <a href={googleSearchUrl} target="_blank" rel="noopener noreferrer">
+//          <button>Search for {searchTerm} Images</button>
+//        </a>
+//      </div>
+//    );
+//  };
 
 // export default Card;
